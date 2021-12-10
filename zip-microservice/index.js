f7b6e85f2e55cd27b41921591e2716262a9a2d8d
Nickie Ferrante <en079702@gmail.com>
2021-12-10 18:56:12 +0700

    Install dependencies and restructure project directories

---
+++ zip-microservice/index.js
@@ -0,0 +1,62 @@
+import express from 'express';
+const app = express();
+import fs from 'fs';
+import bodyParser from 'body-parser';
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization, Content-Length, X-Requested-With, x-access-token, Cache-Control',
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+    try {
+        const { specialID } = req.body;
+
+        console.log(req.body);
+        console.log(specialID);
+
+        if (!specialID) {
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            return res.end('specialID needs to be specified');
+        }
+
+        const specialIDsMap = {
+            one: 1,
+            22: 2,
+            '33threee': 3,
+        };
+        const targetFile = specialIDsMap[specialID];
+        const filePath = `./zips/${targetFile}.zip`;
+        const fileName = 'test-file.zip';
+
+        fs.exists(filePath, (exists) => {
+            if (exists) {
+                res.writeHead(200, {
+                    'Content-Type': 'application/octet-stream',
+                    'Content-Disposition': 'attachment; filename=' + fileName,
+                });
+                fs.createReadStream(filePath).pipe(res);
+            } else {
+                res.writeHead(400, { 'Content-Type': 'text/plain' });
+                res.end('ERROR File does not exist');
+            }
+        });
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.listen(5001);
